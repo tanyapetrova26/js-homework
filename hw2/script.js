b42bddb3b1d4e4adbabe7f711df1210591a74640
Tanya Petrova <tapetrova26@gmail.com>
2022-09-28 18:40:39 +0300

    add index.html

---
+++ hw2/script.js
@@ -0,0 +1,10 @@
+let userAge = 40;
+console.log(userAge);
+
+let userFirstName = 'Tanya';
+console.log(userFirstName);
+
+let userLastName = 'Gromova';
+console.log('userLastName', userLastName);
+
+
