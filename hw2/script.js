570eba7c9e3679a8c8732f80d6e38b9efbc200be
Tanya Petrova <tapetrova26@gmail.com>
2022-09-28 19:36:07 +0300

    add variable

--- hw2/script.js
+++ hw2/script.js
@@ -7,4 +7,27 @@ console.log(userFirstName);
 let userLastName = 'Gromova';
 console.log('userLastName', userLastName);
 
+// новый пользователь
+let newUser = 'Nik';
+console.log('newUser', newUser);
+
+// список покупок
+let shoppingList = 'milk';
+console.log('shoppingList', shoppingList);
+
+shoppingList = 'meat';
+console.log('shoppingList', shoppingList);
+
+// является ли пользователь в сети (online)
+let isUserOnline = true;
+console.log('isUserOnline', isUserOnline);
+
+// зарплата работника
+let employeeSalary = 1000;
+console.log('employeeSalary', employeeSalary);
+
+// координаты курсора
+let cursorCoordinates = 12;
+console.log('cursorCoordinates', cursorCoordinates);
+
 
