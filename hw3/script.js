886e7618d2e2e913ed0336eb6efaa8273be5d4ad
Tanya Petrova <tapetrova26@gmail.com>
2022-10-01 21:02:03 +0300

    task 3

--- hw3/script.js
+++ hw3/script.js
@@ -13,34 +13,70 @@
 
     // task 2
 
-    const num1 = Number(prompt('Введите первое число: '));
-    const num2 = Number(prompt('Введите второе число: '));
-    const action = prompt('Введите желаемое действие: + - / * ');
-    let output;
+    // const num1 = Number(prompt('Введите первое число: '));
+    // const num2 = Number(prompt('Введите второе число: '));
+    // const action = prompt('Введите желаемое действие: + - / * ');
+    // let output;
 
-    switch(action) {
-        case '+':
-            output = num1 + num2;
-            alert(`результат:  ${output}`);
-        break;
+    // switch(action) {
+    //     case '+':
+    //         output = num1 + num2;
+    //         alert(`результат:  ${output}`);
+    //     break;
 
-        case '-':
-            output = num1 - num2;
-            alert(`результат: ${output}`);
-        break;
+    //     case '-':
+    //         output = num1 - num2;
+    //         alert(`результат: ${output}`);
+    //     break;
 
-        case '*':
-            output = num1 * num2;
-            alert(`результат:  ${output}`);
-        break;
+    //     case '*':
+    //         output = num1 * num2;
+    //         alert(`результат:  ${output}`);
+    //     break;
 
-        case '/':
-            output = num1 / num2;
-            alert(`результат: ${output}`);
-        break;
+    //     case '/':
+    //         output = num1 / num2;
+    //         alert(`результат: ${output}`);
+    //     break;
 
-        default:
-            alert(`невозможно произвести действие, введите другое действие`)
-     }
+    //     default:
+    //         alert(`невозможно произвести действие, введите другое действие`)
+    //  }
     
+
+    // task 3
+
+    const visitorAge = Number(prompt('How old are you?'));
+    const teenagerMIN_AGE = 12;
+    const teenagerMAX_AGE = 18;
+    const retireeMIN_AGE = 60;
+    const retireeMAX_AGE = 80;
     
+
+    // if (((visitorAge > teenagerMIN_AGE) &&  (visitorAge < teenagerMAX_AGE)) || 
+    //     ((visitorAge > retireeMIN_AGE) &&  (visitorAge < retireeMAX_AGE))) {
+    //         const isAdultPermission = confirm(`You have permission from an adult? `);
+    //        if (isAdultPermission) {
+            
+    //         alert(`Welcome!`);
+    //        } else{
+    //         alert(`No entry`);
+    //        }
+    //     } else if((visitorAge > teenagerMAX_AGE) && (visitorAge < retireeMIN_AGE)) {
+    //         alert(`Welcome!`);
+    //     } else {
+    //         alert(`No entry`);
+    //     }
+    
+    if((visitorAge >= teenagerMAX_AGE) && (visitorAge <= retireeMIN_AGE)) {
+         alert(`Welcome!`);
+        } else if ((visitorAge < teenagerMIN_AGE) || (visitorAge > retireeMAX_AGE)) {
+            alert(`No entry`);
+            } else {
+                const isAdultPermission = confirm(`You have permission from an adult? `);
+                if (isAdultPermission) {
+                    alert(`Welcome!`);
+                } else {
+                    alert(`No entry`);
+                }
+            }
