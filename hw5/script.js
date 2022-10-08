4f73b0be296ad94ac58c073dd541776c5da5c98e
Tanya Petrova <tapetrova26@gmail.com>
2022-10-08 20:53:13 +0300

    add try counter

--- hw5/script.js
+++ hw5/script.js
@@ -17,6 +17,62 @@ const drawPiramed = (height) => {
     
 }
 
-let  desiredHeight = Number(prompt('Введите высоту пирамиды: '));
+// let  desiredHeight = Number(prompt('Введите высоту пирамиды: '));
 
 // drawPiramed(desiredHeight);
+
+
+const LOGIN = "ADMIN";
+const PASSWORD = "1q2w3e"
+
+const authorise = () => {
+
+    let userLogin;
+    let userPassword;
+    let isAuthSuccess = false;
+    let tryCounter = 0;
+        
+        do {
+            while(tryCounter < 3) {
+                userLogin = prompt("Логин: ");
+                    if(!userLogin){
+                    alert(`Введите логин, у вас осталось ${2 - tryCounter} попытки`);
+                    tryCounter++;
+                    continue;
+                }
+
+                userPassword = prompt("Пароль: ");
+                    if(!userPassword){
+                    alert(`Введите пароль, у вас осталось ${2 - tryCounter} попытки`);
+                    tryCounter++;
+                    continue;
+                }
+
+                if(userLogin === LOGIN && userPassword === PASSWORD) {
+                    isAuthSuccess = true;
+                    break;
+                } else {
+                    alert(`Данные не верны, у вас осталось ${2 - tryCounter} попытки`);
+                    tryCounter++;
+                    continue;
+                }
+
+            }
+            
+        
+            if(tryCounter === 3){
+                alert("Ваши попытки закончились");
+                break;
+            } else {
+                alert("Welcame!");
+            }
+           
+        } while(!isAuthSuccess);
+
+      
+}
+
+authorise();
+
+
+
