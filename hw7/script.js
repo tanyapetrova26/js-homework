56721da77aaaad7c7dbd0626cb2b1153f0796082
Tanya Petrova <tapetrova26@gmail.com>
2022-10-17 18:56:20 +0300

    change task 2

--- hw7/script.js
+++ hw7/script.js
@@ -4,6 +4,7 @@ const salaries = {
     Brown: "1200.50",
     Norman: "1800.30",
     Colyn: "1300.05",
+    Tramp: "3650.54"
 }
 
 function getSalariesTotal(salaries) {
@@ -14,14 +15,14 @@ function getSalariesTotal(salaries) {
     return total;
 }
 
-// let totalRes = getSalariesTotal(salaries);
-// console.log('totalRes', totalRes);
+let totalRes = getSalariesTotal(salaries);
+console.log('totalRes', Math.trunc(totalRes * 100 ) / 100);
 
-// salaries.Neit = "2300.15";
-// salaries.Klark = "5300";
+salaries.Neit = "2300.15";
+salaries.Klark = "5300.80";
 
-// totalRes = getSalariesTotal(salaries);
-// console.log('totalRes', totalRes);
+totalRes = getSalariesTotal(salaries);
+console.log('totalRes', Number(totalRes.toFixed(2)));
 
 
 // расписание
@@ -43,12 +44,11 @@ do {
         Chedule[timeTask] = task;
         
     }
-    
 } while(!getСhedule);
 
 }
 
-showСhedule();
-console.log(Chedule);
+// showСhedule();
+// console.log(Chedule);
 
 
