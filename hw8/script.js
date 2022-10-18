6f963cb110a932dbfb30afa8bb49f52e5a3b7662
Tanya Petrova <tapetrova26@gmail.com>
2022-10-19 00:16:06 +0300

    task 2

--- hw8/script.js
+++ hw8/script.js
@@ -31,6 +31,35 @@ function getMaxSubSum(arr) {
 //   console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
 //   console.log( getMaxSubSum([-5, 0, 4, 5, -9]) ); // 3
 
+const getMinMax = (arr) => {
+    // if (arr.length === 1) {
+    //   return [arr[0], arr[0]]
+    // }
+  
+    const res = {
+      min: arr[0],
+      max: arr[0],
+    };
+  
+    for (let number of arr) {
+      // console.log(`number`, number);
+      if (number < res.min) {
+        res.min = number;
+      }
+  
+      if (number > res.max) {
+        res.max = number;
+      }
+    }
+  
+    return res;
+  };
+  
+//   const minMax = getMinMax([2, 3, 6, 1, -10]);
+//   const minMax = getMinMax([1]);
+//   console.log(`minMax`, minMax)
+
+
 // task 1
 
 function countTrue(arr) {
@@ -43,7 +72,44 @@ function countTrue(arr) {
     }
     return counter;
 }
-console.log(countTrue([true, false, false, true, false])); // -> 2
-console.log(countTrue([false, false, false, false])); // -> 0
-console.log(countTrue([])); // -> 0
-console.log(countTrue([false, false, false, false, true, true, true, true]));
+// console.log(countTrue([true, false, false, true, false])); // -> 2
+// console.log(countTrue([false, false, false, false])); // -> 0
+// console.log(countTrue([])); // -> 0
+// console.log(countTrue([false, false, false, false, true, true, true, true]));
+
+// task 2
+
+function getOccurrencesCount(arr) {
+    const count = {};
+
+    for (const element of arr) {
+        if (count[element]) {
+            count[element] += 1;
+        } else {
+            count[element] = 1;
+        }
+    }
+    return count;
+}
+    console.log(getOccurrencesCount(["a", "v", "a", "b", "b"]));
+// {
+//   a: 2,
+//   v: 1,
+//   b: 2,
+// }
+
+console.log(getOccurrencesCount([
+    "apples",
+    "oranges",
+    "pears",
+    "pears",
+    "apples",
+    "oranges",
+    "oranges",
+    "pears",
+  ]));
+  // {
+  //   apples: 2,
+  //   oranges: 3,
+  //   pears: 3,
+  // }
