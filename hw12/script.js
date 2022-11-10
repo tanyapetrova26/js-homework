0d1b85609b1a299d92a0e28d415ac222ee631906
Tanya Petrova <tapetrova26@gmail.com>
2022-11-10 17:05:36 +0200

    add hw12

---
+++ hw12/script.js
@@ -0,0 +1,22 @@
+
+
+const createTooltip = (element, text) => {
+
+    const tooltip = document.createElement('div');
+    tooltip.classList.add('elemTooltip');
+
+    tooltip.innerText = text;
+
+    document.body.append(tooltip);
+
+    let coords = element.getBoundingClientRect();
+    
+    tooltip.style.left = (coords.left + (tooltip.offsetWidth  - element.offsetWidth) / 2) + 'px';
+    tooltip.style.top = (coords.top - tooltip.offsetHeight - 10) + 'px';
+
+}
+
+
+const button = document.getElementById('example');
+
+createTooltip(button, 'example text bla bla bla');
