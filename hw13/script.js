6b5591d60db2c48682e643c80b261979d93a5657
Tanya Petrova <tapetrova26@gmail.com>
2022-11-14 18:46:22 +0200

    add html

---
+++ hw13/script.js
@@ -0,0 +1,31 @@
+
+const prev = document.querySelector(".button_prev");
+const next = document.querySelector(".button_next");
+
+const slide = document.querySelectorAll(".slide_img, .slide_img-showing");
+
+let currentSlide = 0;
+
+
+function nextSlide() {
+    goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+    goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+    
+    slide[currentSlide].classList = 'slide_img';
+    currentSlide = (n  + slide.length) % slide.length;
+    slide[currentSlide].classList = 'slide_img-showing';
+}
+
+prev.addEventListener('click', () => {
+    previousSlide() 
+});
+
+next.addEventListener('click', () => {
+    nextSlide() 
+});
