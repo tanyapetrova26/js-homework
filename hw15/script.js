5f929d72519b018f6693eb948c0505e227f9c958
Tanya Petrova <tapetrova26@gmail.com>
2022-11-20 22:06:14 +0200

    add task 1

---
+++ hw15/script.js
@@ -0,0 +1,39 @@
+// task 1
+
+const response = {
+    data: [
+      {
+        username: "samuel",
+        is_active: true,
+        created_at: "2020-11-20T09:53:50.000000Z",
+      },
+      {
+        username: "john",
+        is_active: false,
+        created_at: "2020-11-20T09:53:50.000000Z",
+      },
+      {
+        username: "peter",
+        is_active: false,
+        created_at: "2020-11-20T09:53:50.000000Z",
+      },
+    ],
+    meta: {
+      paging: {
+        current: 1,
+        next: 2,
+        prev: null,
+        total: 14,
+        per_page: 3,
+      },
+    },
+  };
+
+  const {meta: {
+            paging: {
+                total: totalPaging
+            }
+        }
+    } = response;
+
+console.log('totalPaging', totalPaging);
