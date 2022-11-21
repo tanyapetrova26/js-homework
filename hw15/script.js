c415b7ea79cd11d085d4072e71b8d855442d614a
Tanya Petrova <tapetrova26@gmail.com>
2022-11-21 22:59:05 +0200

    add task 5

--- hw15/script.js
+++ hw15/script.js
@@ -102,3 +102,67 @@ const user = {
       });
 
     console.log('message', message);
+
+    // task 5.1
+
+    let str = "x1y 722a 333 a123v1n a55555a qwe1 1zxc";
+      
+      
+      const regexpStartLetter = /\b[a-z]\w+\b/g;
+
+          let result = str.match(regexpStartLetter);
+
+          console.log('result', result);
+
+       const regexpEndLetter = /\b\w+[a-z]\b/g;
+
+          result = str.match(regexpEndLetter);
+
+          console.log('result', result);
+
+      const regexpMiddleNumbers = /\b\w\d+\w\b/g;
+
+          result = str.match(regexpMiddleNumbers);
+
+          console.log('result', result);
+
+ // task 5.2
+
+ const validationDomain = (domain) => {
+
+      const validationDomein_regex = /^[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/gi;
+      isValidDomain = false;
+
+      if(validationDomein_regex.test(domain)) {
+        
+          console.log(`Ваш домен ${domain} валидный`);
+          return isValidDomain = true;
+        } else {
+          
+          console.log(`Ваш домен ${domain} невалидный`);
+          return isValidDomain = false;
+        }
+ };
+
+  let resultValidation = validationDomain("ex.ua");
+  console.log('resultValidation', resultValidation);
+
+  // task 5.3
+
+  const checkString = (str)  => { 
+
+      const regexString = /^\d{12,}$/gi;
+      isValidStr = false;
+
+      if(regexString.test(str)) {
+            return isValidStr = true;
+        } else {
+            return isValidStr = false;
+        }
+  };
+
+  let resultValidStr = checkString("1236541111117896hjhj");
+  console.log('resultValidStr', resultValidStr);
+
+
+
