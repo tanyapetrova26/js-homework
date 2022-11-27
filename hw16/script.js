68d9c4878af936f1a33609a31742963a04b471a4
Tanya Petrova <tapetrova26@gmail.com>
2022-11-27 17:58:40 +0200

    add html

---
+++ hw16/script.js
@@ -0,0 +1,50 @@
+
+const renderGreeting = () => {
+    const greeting = document.createElement("h1");
+    greeting.innerText = 'Добро пожаловать';
+  
+    document.body.append(greeting);
+};
+
+const renderNumberVisits = (num) => {
+    const numberVisits = document.createElement("h2");
+    numberVisits.innerText = `Вы заходили раз: ${num}`;
+  
+    document.body.append(numberVisits);
+};
+
+const firstVisit = () => {
+  localStorage.setItem("counter", 1);
+  let counter = localStorage.getItem("counter");
+  
+  renderGreeting();
+  counter++;
+  
+  localStorage.setItem("counter", counter);
+  
+}
+
+  
+  
+const countVisits = () => {
+  counter = localStorage.getItem("counter");
+  
+
+    if (counter == 1) {
+      firstVisit();
+      
+    } else {
+        renderGreeting();
+        renderNumberVisits(counter);
+        counter++;
+        
+        localStorage.setItem("counter", counter);
+        counter = localStorage.getItem("counter");
+        
+      }
+}
+
+countVisits();
+
+
+
